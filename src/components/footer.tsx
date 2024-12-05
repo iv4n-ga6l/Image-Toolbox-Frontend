7fbbe8139c59007a4b9e59add3a6ca4753ccb04c
Ivan <apedoivan@gmail.com>
2024-12-05 13:45:04 +0100

    update links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,19 +9,14 @@ export function Footer() {
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by{' '}
-            <a href="https://github.com/IvanGael" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
-              Ivan APEDO
+            Powered by{' '}
+            <a href="https://nextjs.org/" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
+              NextJS
             </a>.
-            {/* . The source code is available on{' '}
-            <a href="https://github.com/IvanGael" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
-              GitHub
-            </a>
-            . */}
           </p>
         </div>
         <div className="flex items-center space-x-4">
-          <Link href="https://github.com/IvanGael" target="_blank" rel="noreferrer">
+          <Link href="https://github.com/iv4n-ga6l" target="_blank" rel="noreferrer">
             <Github className="h-5 w-5" />
             <span className="sr-only">GitHub</span>
           </Link>
